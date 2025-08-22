0c45312078d9f73fb766540714a02a07507da57e
tezov <tezov.app@gmail.com>
2025-08-23 01:01:37 +0200

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.sql.delight) apply false
+
 }
 
 tasks.register<TestReport>("allUnitTestsDebug") {
