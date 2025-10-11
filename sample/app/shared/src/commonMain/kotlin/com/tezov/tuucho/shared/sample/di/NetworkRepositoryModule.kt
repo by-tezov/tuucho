62be407cdccb9909f03ca7633425fa3d1dc04690
tezov <tezov.app@gmail.com>
2025-10-11 21:19:47 +0200

    create explicite interceptor class

--- sample/app/shared/src/commonMain/kotlin/com/tezov/tuucho/shared/sample/di/NetworkRepositoryModule.kt
+++ sample/app/shared/src/commonMain/kotlin/com/tezov/tuucho/shared/sample/di/NetworkRepositoryModule.kt
@@ -1,47 +1,20 @@
 package com.tezov.tuucho.shared.sample.di
 
 import com.tezov.tuucho.core.data.repository.di.NetworkRepositoryModule
-import com.tezov.tuucho.core.domain.business.protocol.repository.KeyValueStoreRepositoryProtocol.Key.Companion.toKey
-import com.tezov.tuucho.core.domain.business.usecase.GeyValueOrNullFromStoreUseCase
-import com.tezov.tuucho.core.domain.business.usecase._system.UseCaseExecutor
-import io.ktor.client.request.HttpRequestBuilder
-import io.ktor.http.encodedPath
+import com.tezov.tuucho.shared.sample.interceptor.HeaderAuthorizationInterceptor
 import org.koin.dsl.ModuleDeclaration
+import org.koin.dsl.bind
 
 object NetworkRepositoryModule {
 
     fun invoke(): ModuleDeclaration = {
-        factory<NetworkRepositoryModule.RequestInterceptor> {
-            object : NetworkRepositoryModule.RequestInterceptor {
 
-                private val authRegex = Regex("^/auth(?:/.*)?$")
-                private val config = get<NetworkRepositoryModule.Config>()
-                private val useCaseExecutor = get<UseCaseExecutor>()
-                private val geyValueOrNullFromStore = get<GeyValueOrNullFromStoreUseCase>()
-
-                override suspend fun intercept(builder: HttpRequestBuilder) {
-                    val route = builder.url
-                        .encodedPath
-                        .removePrefix("/${config.version}/")
-                        .let {
-                            when  {
-                                it.startsWith(config.healthEndpoint) -> it.removePrefix(config.healthEndpoint)
-                                it.startsWith(config.resourceEndpoint) -> it.removePrefix(config.resourceEndpoint)
-                                it.startsWith(config.sendEndpoint) -> it.removePrefix(config.sendEndpoint)
-                                else -> it
-                            }
-                        }
-                    if (!route.matches(authRegex)) return
-                    useCaseExecutor.invokeSuspend(
-                        useCase = geyValueOrNullFromStore,
-                        input = GeyValueOrNullFromStoreUseCase.Input(
-                            key = "login-authorization".toKey()
-                        )
-                    ).value?.value?.let { authorizationKey ->
-                        builder.headers.append("Authorization", "Bearer $authorizationKey")
-                    }
-                }
-            }
-        }
+        factory {
+            HeaderAuthorizationInterceptor(
+                useCaseExecutor = get(),
+                config = get(),
+                getValueOrNullFromStore = get()
+            )
+        } bind NetworkRepositoryModule.RequestInterceptor::class
     }
 }
---
+++ sample/app/shared/src/commonMain/kotlin/com/tezov/tuucho/shared/sample/interceptor/HeaderAuthorizationInterceptor.kt
@@ -0,0 +1,40 @@
+package com.tezov.tuucho.shared.sample.interceptor
+
+import com.tezov.tuucho.core.data.repository.di.NetworkRepositoryModule
+import com.tezov.tuucho.core.domain.business.protocol.repository.KeyValueStoreRepositoryProtocol.Key.Companion.toKey
+import com.tezov.tuucho.core.domain.business.usecase.GetValueOrNullFromStoreUseCase
+import com.tezov.tuucho.core.domain.business.usecase._system.UseCaseExecutor
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.http.encodedPath
+
+class HeaderAuthorizationInterceptor(
+    private val useCaseExecutor: UseCaseExecutor,
+    private val config: NetworkRepositoryModule.Config,
+    private val getValueOrNullFromStore: GetValueOrNullFromStoreUseCase
+) : NetworkRepositoryModule.RequestInterceptor {
+
+    private val authRegex = Regex("^/auth(?:/.*)?$")
+
+    override suspend fun intercept(builder: HttpRequestBuilder) {
+        val route = builder.url
+            .encodedPath
+            .removePrefix("/${config.version}/")
+            .let {
+                when  {
+                    it.startsWith(config.healthEndpoint) -> it.removePrefix(config.healthEndpoint)
+                    it.startsWith(config.resourceEndpoint) -> it.removePrefix(config.resourceEndpoint)
+                    it.startsWith(config.sendEndpoint) -> it.removePrefix(config.sendEndpoint)
+                    else -> it
+                }
+            }
+        if (!route.matches(authRegex)) return
+        useCaseExecutor.invokeSuspend(
+            useCase = getValueOrNullFromStore,
+            input = GetValueOrNullFromStoreUseCase.Input(
+                key = "login-authorization".toKey()
+            )
+        ).value?.value?.let { authorizationKey ->
+            builder.headers.append("Authorization", "Bearer $authorizationKey")
+        }
+    }
+}
