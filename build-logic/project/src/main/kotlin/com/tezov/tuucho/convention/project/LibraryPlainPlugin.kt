4c78fbabc0f07742a37cb36509ca9e467ed71422
tezov <tezov.app@gmail.com>
2025-10-15 14:51:44 +0200

    #77 - update abi

--- build-logic/project/src/main/kotlin/com/tezov/tuucho/convention/project/LibraryPlainPlugin.kt
+++ build-logic/project/src/main/kotlin/com/tezov/tuucho/convention/project/LibraryPlainPlugin.kt
@@ -1,7 +1,6 @@
 package com.tezov.tuucho.convention.project
 
 import org.gradle.api.Project
-import org.gradle.api.provider.Property
 import org.gradle.api.tasks.testing.Test
 import org.gradle.kotlin.dsl.extra
 import org.gradle.kotlin.dsl.invoke
@@ -11,6 +10,8 @@ import org.gradle.testing.jacoco.plugins.JacocoTaskExtension
 import org.gradle.testing.jacoco.tasks.JacocoReport
 import org.jetbrains.kotlin.allopen.gradle.AllOpenExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.dsl.abi.AbiValidationMultiplatformExtension
+import org.jetbrains.kotlin.gradle.dsl.abi.ExperimentalAbiValidation
 
 open class LibraryPlainPlugin : AbstractLibraryPlugin() {
 
@@ -93,28 +94,15 @@ open class LibraryPlainPlugin : AbstractLibraryPlugin() {
         }
     }
 
+    @OptIn(ExperimentalAbiValidation::class)
     private fun configureApiValidation(project: Project) = with(project) {
         extensions.configure(KotlinMultiplatformExtension::class.java) {
-            // don't know how to access to abiValidation DSL from here. Reflection, allow me to do it...
-            @Suppress("UNCHECKED_CAST")
-            extensions.findByName("abiValidation")?.let { ext ->
-                // enable
-                val enabledProperty = ext.javaClass
-                    .getMethod("getEnabled")
-                    .invoke(ext) as Property<Boolean>
-                enabledProperty.set(true)
-
-                // set folder dir
-                val legacyDump = ext.javaClass
-                    .getMethod("getLegacyDump")
-                    .invoke(ext)
-                val legacyDumpClass = legacyDump.javaClass
-                val refDirProp = legacyDumpClass
-                    .getMethod("getReferenceDumpDir")
-                    .invoke(legacyDump)
-                val rootApiDir = rootProject.layout.projectDirectory.dir("api/${namespace()}")
-                val setMethod = refDirProp.javaClass.getMethod("set", Any::class.java)
-                setMethod.invoke(refDirProp, rootApiDir)
+            extensions.configure(AbiValidationMultiplatformExtension::class.java) {
+                enabled.set(true)
+                legacyDump {
+                    val rootApiDir = rootProject.layout.projectDirectory.dir("api/${namespace()}")
+                    referenceDumpDir.set(rootApiDir)
+                }
             }
         }
     }
