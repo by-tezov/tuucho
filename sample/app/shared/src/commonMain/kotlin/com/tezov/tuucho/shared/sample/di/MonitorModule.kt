ee3a85e15c904a8ecd646b52b968d080464a3825
tezov <tezov.app@gmail.com>
2025-12-01 21:00:06 +0100

    add exception handler

--- sample/app/shared/src/commonMain/kotlin/com/tezov/tuucho/shared/sample/di/MonitorModule.kt
+++ sample/app/shared/src/commonMain/kotlin/com/tezov/tuucho/shared/sample/di/MonitorModule.kt
@@ -4,8 +4,10 @@ import com.tezov.tuucho.core.domain.business.di.ModuleGroupDomain
 import com.tezov.tuucho.core.domain.business.interaction.lock.InteractionLockMonitor
 import com.tezov.tuucho.core.domain.business.protocol.ModuleProtocol.Companion.module
 import com.tezov.tuucho.core.domain.tool.async.CoroutineExceptionMonitor
+import com.tezov.tuucho.core.domain.tool.async.CoroutineUncaughtExceptionHandler
 import com.tezov.tuucho.shared.sample.monitor.LoggerCoroutineExceptionMonitor
 import com.tezov.tuucho.shared.sample.monitor.LoggerInteractionLockMonitor
+import com.tezov.tuucho.shared.sample.uncaughtException.LoggerCoroutineUncaughtExceptionHandler
 import org.koin.core.module.Module
 
 object MonitorModule {
@@ -20,6 +22,13 @@ object MonitorModule {
 
         factory<InteractionLockMonitor> {
             LoggerInteractionLockMonitor(
+                logger = get(),
+                systemInformation = get()
+            )
+        }
+
+        factory<CoroutineUncaughtExceptionHandler> {
+            LoggerCoroutineUncaughtExceptionHandler(
                 logger = get(),
                 systemInformation = get()
             )
---
+++ sample/app/shared/src/commonMain/kotlin/com/tezov/tuucho/shared/sample/uncaughtException/LoggerCoroutineUncaughtExceptionHandler.kt
@@ -0,0 +1,19 @@
+package com.tezov.tuucho.shared.sample.uncaughtException
+
+import com.tezov.tuucho.core.domain.tool.async.CoroutineUncaughtExceptionHandler
+import com.tezov.tuucho.core.domain.tool.protocol.SystemInformationProtocol
+import com.tezov.tuucho.shared.sample._system.Logger
+
+class LoggerCoroutineUncaughtExceptionHandler(
+    private val logger: Logger,
+    private val systemInformation: SystemInformationProtocol
+) : CoroutineUncaughtExceptionHandler {
+
+    override fun process(
+        throwable: Throwable
+    ): Throwable {
+        logger.debug("THREAD") { systemInformation.currentThreadName() }
+        logger.exception("UNCAUGHT", throwable) { "!!!" }
+        return throwable // I want crash xD
+    }
+}
