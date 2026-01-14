1fce75ccc0f2c06e94a8a63735baee02bb3aff16
tezov <tezov.app@gmail.com>
2026-01-14 12:19:37 +0100

    fix ios sample

--- sample/app/shared/src/iosMain/kotlin/com/tezov/tuucho/shared/sample/di/NetworkModule.ios.kt
+++ sample/app/shared/src/iosMain/kotlin/com/tezov/tuucho/shared/sample/di/NetworkModule.ios.kt
@@ -1,14 +1,14 @@
 package com.tezov.tuucho.shared.sample.di
 
-import com.tezov.tuucho.core.barrel.di.ModuleGroupCore
-import com.tezov.tuucho.core.domain.business.di.Koin.Companion.module
+import com.tezov.tuucho.core.barrel.di.ModuleContextCore
+import com.tezov.tuucho.core.domain.business.di.KoinMass.Companion.module
 import io.ktor.client.engine.HttpClientEngineFactory
 import io.ktor.client.engine.darwin.Darwin
 
 internal object NetworkModuleIos {
 
     object FlavorDefault {
-        fun invoke() = module(ModuleGroupCore.Main) {
+        fun invoke() = module(ModuleContextCore.Main) {
             factory<HttpClientEngineFactory<*>> {
                 Darwin
             }
--- sample/app/shared/src/iosMain/kotlin/com/tezov/tuucho/shared/sample/di/SystemSharedModules.ios.kt
+++ sample/app/shared/src/iosMain/kotlin/com/tezov/tuucho/shared/sample/di/SystemSharedModules.ios.kt
@@ -1,7 +1,7 @@
 package com.tezov.tuucho.shared.sample.di
 
-import com.tezov.tuucho.core.domain.business.di.Koin
-internal actual fun SystemSharedModules.platformInvoke(): List<Koin> = listOf(
+import com.tezov.tuucho.core.domain.business.di.KoinMass
+internal actual fun SystemSharedModules.platformInvoke(): List<KoinMass> = listOf(
     ConfigModuleIos.invoke(),
     NetworkModuleIos.invoke(),
 )
