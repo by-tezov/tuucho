989d83cd844e44724bc7299f966423d2e8769483
tezov <tezov.app@gmail.com>
2026-01-15 22:15:52 +0100

    #132 - chore/ fix missing shadower

---
+++ tuucho/core-modules/data/repository/src/commonMain/kotlin/com/tezov/tuucho/core/data/repository/parser/shadower/StyleShadower.kt
@@ -0,0 +1,30 @@
+package com.tezov.tuucho.core.data.repository.parser.shadower
+
+import com.tezov.tuucho.core.data.repository.parser._system.isTypeOf
+import com.tezov.tuucho.core.data.repository.parser.shadower._system.ShadowerMatcherProtocol
+import com.tezov.tuucho.core.data.repository.parser.shadower._system.ShadowerProtocol
+import com.tezov.tuucho.core.domain.business._system.koin.AssociateDSL.getAllAssociated
+import com.tezov.tuucho.core.domain.business.jsonSchema.material.TypeSchema
+import com.tezov.tuucho.core.domain.tool.json.JsonElementPath
+import kotlinx.serialization.json.JsonElement
+
+class StyleShadower : AbstractShadower() {
+    sealed class Association {
+        object Matcher : Association()
+
+        object Processor : Association()
+    }
+
+    override val matchers: List<ShadowerMatcherProtocol> by lazy {
+        getKoin().getAllAssociated(Association.Matcher::class)
+    }
+
+    override val childProcessors: List<ShadowerProtocol> by lazy {
+        getKoin().getAllAssociated(Association.Processor::class)
+    }
+
+    override fun accept(
+        path: JsonElementPath,
+        element: JsonElement,
+    ) = path.isTypeOf(element, TypeSchema.Value.style) || super.accept(path, element)
+}
