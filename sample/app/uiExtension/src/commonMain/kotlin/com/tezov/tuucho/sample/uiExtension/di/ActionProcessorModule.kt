85f3844fef4e0a4c80062577da982770885b53c8
tezov <tezov.app@gmail.com>
2026-01-30 23:30:55 +0100

    fix koin bind object

--- sample/app/uiExtension/src/commonMain/kotlin/com/tezov/tuucho/sample/uiExtension/di/ActionProcessorModule.kt
+++ sample/app/uiExtension/src/commonMain/kotlin/com/tezov/tuucho/sample/uiExtension/di/ActionProcessorModule.kt
@@ -1,16 +1,13 @@
 package com.tezov.tuucho.sample.uiExtension.di
 
-import com.tezov.tuucho.core.domain.business._system.koin.Associate.associate
 import com.tezov.tuucho.core.domain.business._system.koin.KoinMass.Companion.module
 import com.tezov.tuucho.core.domain.business._system.koin.KoinModuleExtension.factoryObject
 import com.tezov.tuucho.core.domain.business.di.ModuleContextDomain
 import com.tezov.tuucho.core.domain.business.middleware.ActionMiddleware
-import com.tezov.tuucho.core.domain.business.model.action.NavigateActionDefinition
 import com.tezov.tuucho.core.domain.business.protocol.ActionDefinitionProtocol
 import com.tezov.tuucho.sample.uiExtension.domain.EchoMessageCustomActionDefinition
 import com.tezov.tuucho.sample.uiExtension.domain.EchoMessageCustomActionMiddleware
 import org.koin.core.module.dsl.factoryOf
-import org.koin.core.qualifier.named
 import org.koin.dsl.bind
 
 internal object ActionProcessorModule {
