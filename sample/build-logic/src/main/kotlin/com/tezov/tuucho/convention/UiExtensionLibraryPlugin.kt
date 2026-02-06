10221b1ed8ed6d1b25f88b137f3505226b865053
tezov <tezov.app@gmail.com>
2026-02-06 10:23:34 +0100

    fix preview

--- sample/build-logic/src/main/kotlin/com/tezov/tuucho/convention/UiExtensionLibraryPlugin.kt
+++ sample/build-logic/src/main/kotlin/com/tezov/tuucho/convention/UiExtensionLibraryPlugin.kt
@@ -1,5 +1,12 @@
 package com.tezov.tuucho.convention
 
+import com.tezov.tuucho.convention._system.LibraryId
+import com.tezov.tuucho.convention._system.library
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.invoke
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+
 class UiExtensionLibraryPlugin : AbstractLibraryPlugin() {
 
     override fun optIn() = listOf<String>(
@@ -12,5 +19,43 @@ class UiExtensionLibraryPlugin : AbstractLibraryPlugin() {
 
     override val hasAssets = false
 
+    override fun configure(
+        project: Project,
+    ) {
+        super.configure(project)
+        with(project) {
+            configurePreviewDependencies()
+            configurePreviewSourceSet()
+        }
+    }
+
+    private fun Project.configurePreviewDependencies() {
+        extensions.configure(KotlinMultiplatformExtension::class.java) {
+            sourceSets {
+                androidMain.dependencies {
+                    implementation(library(LibraryId.composeUiTooling))
+                }
+            }
+        }
+        dependencies {
+            add(
+                "androidRuntimeClasspath",
+                library(LibraryId.composeUiToolingPreview)
+            )
+        }
+    }
+
+    private fun Project.configurePreviewSourceSet() {
+        val folder = "preview"
+        extensions.configure(KotlinMultiplatformExtension::class.java) {
+            sourceSets {
+                androidMain {
+                    kotlin.srcDirs(
+                        "${project.projectDir.path}/src/androidMain/$folder"
+                    )
+                }
+            }
+        }
+    }
 }
 
--- tuucho/build-logic/project/src/main/kotlin/com/tezov/tuucho/convention/project/LibraryUiPlugin.kt
+++ tuucho/build-logic/project/src/main/kotlin/com/tezov/tuucho/convention/project/LibraryUiPlugin.kt
@@ -1,16 +1,66 @@
 package com.tezov.tuucho.convention.project
 
+import com.tezov.tuucho.convention.project._system.LibraryId
 import com.tezov.tuucho.convention.project._system.PluginId
+import com.tezov.tuucho.convention.project._system.buildType
+import com.tezov.tuucho.convention.project._system.library
 import com.tezov.tuucho.convention.project._system.plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.invoke
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
 class LibraryUiPlugin : LibraryPlainPlugin() {
+
+    private val Project.shouldConfigurePreview
+        get() = buildType() == "debug"
 
     override fun applyPlugins(project: Project) {
         super.applyPlugins(project)
         with(project) {
             pluginManager.apply(plugin(PluginId.compose))
             pluginManager.apply(plugin(PluginId.composeCompiler))
+        }
+    }
+
+    override fun configure(
+        project: Project,
+    ) {
+        super.configure(project)
+        with(project) {
+            if (shouldConfigurePreview) {
+                configurePreviewDependencies()
+                configurePreviewSourceSet()
+            }
+        }
+    }
+
+    private fun Project.configurePreviewDependencies() {
+        extensions.configure(KotlinMultiplatformExtension::class.java) {
+            sourceSets {
+                androidMain.dependencies {
+                    implementation(library(LibraryId.composeUiTooling))
+                }
+            }
+        }
+        dependencies {
+            add(
+                "androidRuntimeClasspath",
+                library(LibraryId.composeUiToolingPreview)
+            )
+        }
+    }
+
+    private fun Project.configurePreviewSourceSet() {
+        val folder = "preview"
+        extensions.configure(KotlinMultiplatformExtension::class.java) {
+            sourceSets {
+                androidMain {
+                    kotlin.srcDirs(
+                        "${project.projectDir.path}/src/androidMain/$folder"
+                    )
+                }
+            }
         }
     }
 }
